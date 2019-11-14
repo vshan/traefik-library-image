851ed5421f8802d8f2a3818586675b7f1ffbdd71
Vinay Bhat <vibha@microsoft.com>
2019-11-14 16:18:27 +0530

    Hydra Traefik Image

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -15,4 +15,13 @@ else
     echo "= '$1' is not a Traefik command: assuming shell execution." 1>&2
 fi
 
+mkdir -p $(dirname ${TRAEFIK_PROVIDERS_FILE_FILENAME})
+
+while true; do
+	cp -rf "${TRAEFIK_FILE_MOUNT_DIR}" $(dirname $(dirname ${TRAEFIK_PROVIDERS_FILE_FILENAME}))
+        sleep 30
+done &
+
+./initfilelock "TRAEFIK_PROVIDERS_FILE_FILENAME" "120" > initfilelock.log
+
 exec "$@"
